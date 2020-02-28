b6add32d0ccf60a24a76e25945c2c1e175b02023
walter <wfilho182@gmail.com>
2020-02-28 09:04:00 -0300

    parte 2 mermããããão
    naquele pique

--- exercicio1.java
+++ exercicio1.java
@@ -2,5 +2,10 @@ class exercicio1 {
     public static void main (String []arg) {
         for (int i = 150; i<=300; i++)
         System.out.println(i);
+        
+        int soma=0;
+        for (int j=0; j<=1000; j ++)
+        soma  = soma + j;
+        System.out.println(soma);
     } 
 }
